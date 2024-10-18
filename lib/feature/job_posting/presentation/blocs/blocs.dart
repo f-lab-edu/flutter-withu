708b3a1f8c6319d50217084bedee336d998f394e
Conner <dudrjs145@naver.com>
2024-10-18 10:37:13 +0900

    feat: 공고 상세 화면 생성 및 연결 (#4)
    1. JobPostingDetailPage 생성
    2. JobPostingPage에서 리스트 아이템 클릭 시 화면 이동 추가

--- lib/feature/job_posting/presentation/blocs/blocs.dart
+++ lib/feature/job_posting/presentation/blocs/blocs.dart
@@ -1,3 +1,4 @@
 export 'job_postings/job_postings_bloc.dart';
 export 'job_postings_tab/job_postings_tab.dart';
 export 'form/job_posting_form_bloc.dart';
+export 'detail/job_posting_detail_bloc.dart';
