2775875b11932839fb3099c8402304e39a53eddc
Conner <dudrjs145@naver.com>
2024-10-30 17:29:04 +0900

    feat: #16/로그인 Repository 추가

--- lib/feature/account/data/repository/repository_impl.dart
+++ lib/feature/account/data/repository/repository_impl.dart
@@ -1,7 +1,20 @@
+import 'dart:async';
+
+import 'package:withu_app/core/core.dart';
 import 'package:withu_app/feature/account/account.dart';
 
 class AccountRepositoryImpl implements AccountRepository {
+  @override
   final AccountApi accountApi;
 
-  AccountRepositoryImpl({required this.accountApi});
+  AccountRepositoryImpl({
+    required this.accountApi,
+  });
+
+  @override
+  FutureOr<ApiResponse<LoginResponseDto>> login({
+    required LoginRequestDto requestData,
+  }) async {
+    return await accountApi.login(requestData: requestData);
+  }
 }
--- lib/feature/account/domain/repository/repository.dart
+++ lib/feature/account/domain/repository/repository.dart
@@ -1,1 +1,14 @@
-abstract class AccountRepository {}
+import 'dart:async';
+
+import 'package:withu_app/core/core.dart';
+import 'package:withu_app/feature/account/data/data.dart';
+
+abstract class AccountRepository {
+  final AccountApi accountApi;
+
+  AccountRepository({required this.accountApi});
+
+  FutureOr<ApiResponse<LoginResponseDto>> login({
+    required LoginRequestDto requestData,
+  });
+}
