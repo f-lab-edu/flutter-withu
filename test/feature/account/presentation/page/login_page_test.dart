e3b6ead668e4f18110b60d1b1ab5c6028604a2ea
Conner <dudrjs145@naver.com>
2024-11-03 15:00:40 +0900

    test: 위젯 테스트에서 Bloc에 관련 테스트 삭제

--- test/feature/account/presentation/page/login_page_test.dart
+++ test/feature/account/presentation/page/login_page_test.dart
@@ -1,40 +1,32 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
 import 'package:withu_app/core/core.dart';
 import 'package:withu_app/feature/account/account.dart';
 import 'package:withu_app/shared/shared.dart';
 
-import 'login_page_test.mocks.dart';
 import 'login_page_test_helper.dart';
 
-@GenerateMocks([LoginBloc])
+class MockLoginBloc extends MockBloc<LoginEvent, LoginState>
+    implements LoginBloc {}
+
 void main() {
   group('LoginPage Test', () {
     late MockLoginBloc loginBloc;
     late Widget testWidget;
+    late LoginState initialState;
 
     setUp(() {
       loginBloc = MockLoginBloc();
 
-      final initialState = LoginState(
+      initialState = LoginState(
         status: BaseBlocStatus.initial(),
-        selectedTab: AccountType.company,
-      );
-
-      when(loginBloc.state).thenReturn(initialState);
-      when(loginBloc.stream).thenAnswer(
-        (_) => Stream.fromIterable([
-          initialState,
-          initialState.copyWith(selectedTab: AccountType.user),
-        ]),
       );
 
       testWidget = MaterialApp(
-        home: BlocProvider<LoginBloc>.value(
-          value: loginBloc,
+        home: BlocProvider<LoginBloc>(
+          create: (context) => loginBloc,
           child: const LoginPageContent(), // 로그인 페이지 위젯
         ),
       );
@@ -42,23 +34,18 @@ void main() {
 
     testWidgets('화면 로딩 후 초기화 상태 검사', (WidgetTester tester) async {
       //Given
+      whenListen(
+        loginBloc,
+        Stream.fromIterable([initialState]),
+        initialState: initialState,
+      );
+
+      // When
       await tester.pumpWidget(testWidget);
       final companyTab = LoginPageTestHelper.getCompanyTab(tester);
       final userTab = LoginPageTestHelper.getUserTab(tester);
 
-      // When
-
-      // Then
-      expect(loginBloc.state.status.isInitial, true);
-      expect(loginBloc.state.selectedTab, AccountType.company);
-      expect(loginBloc.state.loginId, '');
-      expect(loginBloc.state.password, '');
-      expect(loginBloc.state.isValidId, true);
-      expect(loginBloc.state.isValidPassword, true);
-      expect(loginBloc.state.isEnabledLogin, false);
-
       // UI 요소 검증;
-
       expect(companyTab, isA<BaseTab>());
       expect(companyTab.isSelected, true);
       expect(userTab, isA<BaseTab>());
@@ -69,68 +56,50 @@ void main() {
 
     testWidgets('새로운 일 찾기 탭 클릭 테스트', (WidgetTester tester) async {
       //Given
-      await tester.pumpWidget(testWidget);
-      final companyTab = LoginPageTestHelper.getCompanyTab(tester);
-      final userTab = LoginPageTestHelper.getUserTab(tester);
-
-      when(
-        loginBloc.add(argThat(isA<LoginTabPressed>())),
-      ).thenAnswer((_) {
-        when(loginBloc.state).thenReturn(
-          LoginState(
-            status: BaseBlocStatus.initial(),
+      whenListen(
+        loginBloc,
+        Stream.fromIterable([
+          initialState,
+          initialState.copyWith(
             selectedTab: AccountType.user,
-          ),
-        );
-      });
-
-      // 초기 상태 검증
-      expect(companyTab.isSelected, true);
-      expect(userTab.isSelected, false);
+          )
+        ]),
+        initialState: initialState,
+      );
 
       // When
+      await tester.pumpWidget(testWidget);
       await tester.tap(LoginPageTestHelper.userTabFinder());
       await tester.pumpAndSettle();
 
       // Then
-      verify(loginBloc.add(argThat(isA<LoginTabPressed>()))).called(1);
       expect(loginBloc.state.selectedTab, equals(AccountType.user));
-
-      final updatedCompanyTab = LoginPageTestHelper.getCompanyTab(tester);
-      final updatedUserTab = LoginPageTestHelper.getUserTab(tester);
-      expect(updatedCompanyTab.isSelected, false);
-      expect(updatedUserTab.isSelected, true);
+      expect(LoginPageTestHelper.getCompanyTab(tester).isSelected, false);
+      expect(LoginPageTestHelper.getUserTab(tester).isSelected, true);
     });
 
     testWidgets('이메일 입력 테스트', (WidgetTester tester) async {
       //Given
-      const loginId = 'test@test.com';
-      when(
-        loginBloc.add(argThat(isA<LoginIdInputted>())),
-      ).thenAnswer((_) {
-        when(loginBloc.state).thenReturn(
-          loginBloc.state.copyWith(
-            loginId: loginId,
-            isValidId: true,
-          ),
-        );
-      });
-
-      await tester.pumpWidget(testWidget);
-
-      // 초기 상태 검증
-      expect(loginBloc.state.loginId, '');
+      whenListen(
+        loginBloc,
+        Stream.fromIterable([
+          initialState,
+          initialState.copyWith(
+            selectedTab: AccountType.user,
+          )
+        ]),
+        initialState: initialState,
+      );
 
       // When
-      await tester.enterText(
-        LoginPageTestHelper.idInputFinder(),
-        loginId,
-      );
+      await tester.pumpWidget(testWidget);
+      await tester.tap(LoginPageTestHelper.userTabFinder());
       await tester.pumpAndSettle();
 
       // Then
-      verify(loginBloc.add(argThat(isA<LoginIdInputted>()))).called(1);
-      expect(find.text(loginId), findsOneWidget);
+      expect(loginBloc.state.selectedTab, equals(AccountType.user));
+      expect(LoginPageTestHelper.getCompanyTab(tester).isSelected, false);
+      expect(LoginPageTestHelper.getUserTab(tester).isSelected, true);
     });
   });
 }
